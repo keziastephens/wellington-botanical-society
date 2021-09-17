23e9222a790c8b973e94601e846f61aab25559c7
keziastephens <87955878+keziastephens@users.noreply.github.com>
2021-09-17 16:50:15 +1200

    Add files via upload

---
+++ wbs-sum1/script.js
@@ -0,0 +1,9 @@
+    console.log("hello");
+
+    // $("#nav-mobile").hide(); hide an element
+    // $("#nav-mobile").show(); show an elements
+
+    $("#hamburger").click(function(){
+        $("#nav-mobile").toggle();
+    });
+    
